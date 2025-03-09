612b6229cfdace4bf22cd5b5f21c2c1060229077
Tahseen Dar <tahseendar01@gmail.com>
2025-03-09 13:32:47 +0000

    changes to the server production file

--- backend/server.js
+++ backend/server.js
@@ -49,13 +49,13 @@ app.use("/uploads", express.static(path.join(__dirname)));
 // console.log(path.join(__dirname, "../frontend/build"));
 // console.log(path.resolve(__dirname, "../frontend", "build", "index.html"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
-  );
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
+//   );
+// }
 app.get("/", (req, res) => {
   res.send("API is running....");
 });
